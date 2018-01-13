e88b0c7beeb733762733b8dbee1127c05f831132
Doug Johnson <dougvj@gmail.com>
2018-01-13 16:24:14 -0700

    UI KeyEvent: Fix AND masks for repeat_count and prev_state
    Commits f5104af0a2c2266e4ce02db8b73999cadd377fcd and
    38064acd517e6dec29acf19f9b975107729f6ea1 appear to have put in the wrong
    AND masks for repeat_count and prev_state this entire time, according to
    https://msdn.microsoft.com/en-us/library/windows/desktop/ms646280(v=vs.85).aspx

--- src/xenia/ui/window_win.cc
+++ src/xenia/ui/window_win.cc
@@ -682,7 +682,7 @@ bool Win32Window::HandleMouse(UINT message, WPARAM wParam, LPARAM lParam) {
 
 bool Win32Window::HandleKeyboard(UINT message, WPARAM wParam, LPARAM lParam) {
   auto e = KeyEvent(
-      this, static_cast<int>(wParam), lParam & 0xFFFF0000, !!(lParam & 0x2),
+      this, static_cast<int>(wParam), lParam & 0xFFFF, !!(lParam & 0x40000000),
       !!(GetKeyState(VK_SHIFT) & 0x80), !!(GetKeyState(VK_CONTROL) & 0x80),
       !!(GetKeyState(VK_MENU) & 0x80), !!(GetKeyState(VK_LWIN) & 0x80));
   switch (message) {
